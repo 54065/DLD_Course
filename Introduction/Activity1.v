acfc5ebef8b9df5dce7fe39d876ff0ba4869f7dd
54065 <usamaibrar19@gmail.com>
2026-06-21 16:01:11 +0500

    Code of first activity

--- Introduction/Activity1.v
+++ Introduction/Activity1.v
@@ -20,9 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module INTRODUCTION_TO_DLD(
-input a,b 
-output c
-
+module Digital_system(
+input power_in, manual_in, 
+input [6:0] temp_set,temp_sensor,
+output power_out, FAN
     );
+reg FAN;   
+    
+    assign power_out = power_in;   
+    always @ (power_in, temp_sensor, manual_in, temp_set) begin
+        if (power_in) begin 
+         
+            if (temp_sensor >= temp_set || manual_in == 1) begin
+                FAN = 1;
+                end
+            
+            else begin
+            FAN = 0;
+            end
+        end
+        
+        else begin
+            FAN = 0;
+        end
+   end
+  
 endmodule
